0a0d9c6e831a35a9b4a7fd356ca344a7b68291a1
3mlnssaco <116640413+3mlnssaco@users.noreply.github.com>
2024-06-30 05:00:42 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,58 @@
-import 'package:flutter/material.dart';
-import 'crypto_service.dart';
-import 'all_coins.dart';
-import 'favorite_stocks_screen.dart';
-import 'search_coins.dart';
+import 'package:flutter/material.dart'; // Flutter의 UI 요소를 위한 패키지
+import 'crypto_service.dart'; // CryptoService 클래스를 가져오기 위한 패키지
+import 'all_coins.dart'; // SearchCoinScreen 클래스를 가져오기 위한 패키지
+import 'favorite_stocks_screen.dart'; // FavoriteStocksScreen 클래스를 가져오기 위한 패키지
+import 'search_coins.dart'; // PreferencesScreen 클래스를 가져오기 위한 패키지
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final cryptoService = CryptoService();
-  await cryptoService.initialize();
-  runApp(MyApp(cryptoService: cryptoService));
+  WidgetsFlutterBinding.ensureInitialized(); // Flutter 위젯 바인딩 초기화
+  final cryptoService = CryptoService(); // CryptoService 인스턴스 생성
+  await cryptoService.initialize(); // CryptoService 초기화
+  runApp(MyApp(cryptoService: cryptoService)); // MyApp 실행
 }
 
+// MyApp 클래스 정의
 class MyApp extends StatelessWidget {
-  final CryptoService cryptoService;
+  final CryptoService cryptoService; // CryptoService 인스턴스 변수 선언
 
-  const MyApp({super.key, required this.cryptoService});
+  const MyApp({super.key, required this.cryptoService}); // 생성자 정의
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Crypto App',
+      title: 'Crypto App', // 앱 제목 설정
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue, // 앱 테마 색상 설정
       ),
-      home: MyHomePage(cryptoService: cryptoService),
+      home: MyHomePage(cryptoService: cryptoService), // MyHomePage로 이동
     );
   }
 }
 
+// MyHomePage 클래스 정의
 class MyHomePage extends StatefulWidget {
-  final CryptoService cryptoService;
+  final CryptoService cryptoService; // CryptoService 인스턴스 변수 선언
 
-  const MyHomePage({super.key, required this.cryptoService});
+  const MyHomePage({super.key, required this.cryptoService}); // 생성자 정의
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState(); // 상태 클래스 생성
 }
 
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
-  late TabController _tabController;
+// _MyHomePageState 클래스 정의
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController; // TabController 인스턴스 변수 선언
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
+    _tabController = TabController(length: 3, vsync: this); // TabController 초기화
   }
 
   @override
   void dispose() {
-    _tabController.dispose();
+    _tabController.dispose(); // TabController 해제
     super.dispose();
   }
 
@@ -56,22 +60,22 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Crypto App'),
+        title: const Text('Crypto App'), // 앱바 제목 설정
         bottom: TabBar(
-          controller: _tabController,
+          controller: _tabController, // TabController 설정
           tabs: const [
-            Tab(icon: Icon(Icons.search), text: 'Search Coin'),
-            Tab(icon: Icon(Icons.list), text: 'All Coins'),
-            Tab(icon: Icon(Icons.star), text: 'Favorites'),
+            Tab(icon: Icon(Icons.search), text: 'Search Coin'), // 탭 아이콘과 텍스트 설정
+            Tab(icon: Icon(Icons.list), text: 'All Coins'), // 탭 아이콘과 텍스트 설정
+            Tab(icon: Icon(Icons.star), text: 'Favorites'), // 탭 아이콘과 텍스트 설정
           ],
         ),
       ),
       body: TabBarView(
-        controller: _tabController,
+        controller: _tabController, // TabController 설정
         children: [
           SearchCoins(cryptoService: widget.cryptoService),
-          SearchCoinScreen(cryptoService: widget.cryptoService),
-          FavoriteStocksScreen(cryptoService: widget.cryptoService),
+          AllCoinsScreen(cryptoService: widget.cryptoService), // SearchCoinScreen 설정
+          FavoriteStocksScreen(cryptoService: widget.cryptoService), // FavoriteStocksScreen 설정
         ],
       ),
     );
